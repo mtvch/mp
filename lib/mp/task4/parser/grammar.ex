18d6d63f3443ba9122b161d533cf0822f2a436b3
Матвей Карпов <matvey.karpov@balance-pl.ru>
2024-01-02 22:05:16 +0700

    FIX: Task4: Grammar

--- lib/mp/task4/parser/grammar.ex
+++ lib/mp/task4/parser/grammar.ex
@@ -8,19 +8,18 @@ defmodule Mp.Task4.Parser.Grammar do
   Возвращает PEG граматику языка выражений
   """
   def peg do
-    peg L1 do
+    peg Expr do
       S <- star({' ', '\t', '\r', '\n'})
-      L1 <- S * L2 * star(L1Op) * S
-      L2 <- L3 * S * star(L2Op) * S
-      L3 <- NotOp | Const | Var | "(" * S * L1 * S * ")"
-      NotOp <- str("!") * L3 * fn [x, _op | cs] -> [%Mp.Task4.AstNodes.Not{x: x} | cs] end
+      Expr <- S * Term * S * star(BinOp) * S
+      Term <- NotOp | Const | Var | "(" * S * Expr * S * ")"
+      NotOp <- str("!") * Term * fn [x, _op | cs] -> [%Mp.Task4.AstNodes.Not{x: x} | cs] end
       Const <- int({'0', '1'}) * fn [x | cs] -> [%Mp.Task4.AstNodes.Const{value: x} | cs] end
       Var <- str({'a'..'z'}) * fn [x | cs] -> [%Mp.Task4.AstNodes.Var{name: x} | cs] end
-      L1Op <- str("->") * S * L1 * fn [b, _op, a | cs] -> [%Mp.Task4.AstNodes.Implication{a: a, b: b} | cs] end
-      L2Op <- str({'&', '|'}) * S * L1 * fn [b, op, a | cs] ->
+      BinOp <- (str({'&', '|'}) | str("->")) * S * Expr * fn [b, op, a | cs] ->
             case op do
               "&" -> [%Mp.Task4.AstNodes.And{a: a, b: b} | cs]
               "|" -> [%Mp.Task4.AstNodes.Or{a: a, b: b} | cs]
+              "->" -> [%Mp.Task4.AstNodes.Implication{a: a, b: b} | cs]
             end
           end
     end
